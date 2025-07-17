f9c4e4d1404cccbb151a6788c7f59d25b0e50945
Setsuna@MBP2023WIN11 <yukishita.mfg@gmail.com>
2025-07-17 23:55:18 +0900

    #96 未使用の変数を削除、コメントの体裁整理

--- Code/CoRectSys/CollectionDataClass.cs
+++ Code/CoRectSys/CollectionDataClass.cs
@@ -821,7 +821,7 @@ namespace CREC
             }
 
             // バックアップ数管理処理（バックアップ作成後に実行）
-            ManageBackupCount(projectSettingValues, collectionDataValues, backupFolderPath);
+            ManageBackupCount(projectSettingValues, backupFolderPath);
 
             return true;
         }
@@ -830,11 +830,9 @@ namespace CREC
         /// 指定したコレクションのバックアップ数を管理し、古いバックアップを削除する
         /// </summary>
         /// <param name="projectSettingValues">プロジェクト設定値</param>
-        /// <param name="collectionDataValues">コレクションデータ値</param>
         /// <param name="backupFolderPath">バックアップフォルダのパス</param>
         private static void ManageBackupCount(
             ProjectSettingValuesClass projectSettingValues,
-            CollectionDataValuesClass collectionDataValues,
             string backupFolderPath)
         {
             try
@@ -844,60 +842,56 @@ namespace CREC
                 {
                     return;
                 }
-
-                // バックアップファイル/フォルダのリストを取得
-                List<FileSystemInfo> backupItems = new List<FileSystemInfo>();
                 
+                List<FileSystemInfo> backupItems = new List<FileSystemInfo>();// バックアップファイル/フォルダのリストを取得
+
                 // 圧縮設定に応じてファイルまたはフォルダを取得
                 switch (projectSettingValues.BackupCompressionType)
                 {
-                    case BackupCompressionType.NoCompress:
-                        // フォルダバックアップの場合
+                    case BackupCompressionType.NoCompress:// フォルダバックアップの場合
                         DirectoryInfo backupDir = new DirectoryInfo(backupFolderPath);
                         backupItems.AddRange(backupDir.GetDirectories());
                         break;
-                    case BackupCompressionType.Zip:
-                        // Zipファイルバックアップの場合
+                    case BackupCompressionType.Zip:// Zipファイルバックアップの場合
                         DirectoryInfo backupDirZip = new DirectoryInfo(backupFolderPath);
                         backupItems.AddRange(backupDirZip.GetFiles("*.zip"));
                         break;
                 }
 
-                // 作成日時順でソート（古い順）
-                backupItems.Sort((x, y) => x.CreationTime.CompareTo(y.CreationTime));
+                backupItems.Sort((x, y) => x.CreationTime.CompareTo(y.CreationTime));// 作成日時順でソート（古い順）
+
+                // バックアップ数を取得
+                int maxBackupCount = projectSettingValues.MaxBackupCount;
+                if (maxBackupCount < 1)
+                {
+                    maxBackupCount = 1; // 最小値は1
+                }
 
                 // 設定された最大バックアップ数を超えている場合、古いものから削除
-                int maxBackupCount = projectSettingValues.MaxBackupCount;
-                if (maxBackupCount < 1) maxBackupCount = 1; // 最小値は1
-
                 while (backupItems.Count > maxBackupCount)
                 {
-                    FileSystemInfo oldestBackup = backupItems[0];
-                    backupItems.RemoveAt(0);
+                    FileSystemInfo oldestBackup = backupItems[0];// 最も古いバックアップをリストから取得
+                    backupItems.RemoveAt(0);// 最も古いバックアップをリストから削除
 
                     try
                     {
-                        if (oldestBackup is DirectoryInfo)
+                        if (oldestBackup is DirectoryInfo)// フォルダの場合
                         {
-                            // フォルダの場合
                             Directory.Delete(oldestBackup.FullName, true);
                         }
-                        else if (oldestBackup is FileInfo)
+                        else if (oldestBackup is FileInfo)// ファイルの場合
                         {
-                            // ファイルの場合
                             File.Delete(oldestBackup.FullName);
                         }
                     }
-                    catch (Exception ex)
+                    catch (Exception ex)// 個別のバックアップ削除失敗は警告のみ表示して続行
                     {
-                        // 個別のバックアップ削除失敗は警告のみ表示して続行
                         MessageBox.Show($"古いバックアップの削除に失敗しました: {oldestBackup.Name}\n{ex.Message}", "CREC");
                     }
                 }
             }
-            catch (Exception ex)
+            catch (Exception ex)// バックアップ管理処理の失敗は警告のみ表示
             {
-                // バックアップ管理処理の失敗は警告のみ表示
                 MessageBox.Show($"バックアップ数管理処理でエラーが発生しました: {ex.Message}", "CREC");
             }
         }
