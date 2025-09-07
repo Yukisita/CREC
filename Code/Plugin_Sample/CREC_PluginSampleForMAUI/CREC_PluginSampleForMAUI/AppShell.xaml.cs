e0665709b30484c6a7e466733e26297252c84717
Setsuna@MBP2023WIN11 <yukishita.mfg@gmail.com>
2025-09-07 20:30:48 +0900

    #127 #128 プラグインのサンプルプログラムを作成（MAUI、exe実行ファイル）

---
+++ Code/Plugin_Sample/CREC_PluginSampleForMAUI/CREC_PluginSampleForMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CREC_PluginSampleForMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Code/Plugin_Sample/CREC_PluginSampleForMAUI/CREC_PluginSampleForMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CREC_PluginSampleForMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
